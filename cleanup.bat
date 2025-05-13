f3d1b157b1cd8fdb4ed9a6af53ea2a109312c570
Quinten1508 <quinten1508@gmail.com>
2025-05-13 14:31:45 +0200

    Restore cleanup.bat file

---
+++ cleanup.bat
@@ -0,0 +1,12 @@
+@echo off
+cd flask-app
+del create_sample_entry.py
+del run_ecs_exec.cmd
+del check_db_via_ecs.py
+del check_inside_container.py
+del export_architecture.py
+del query_db.py
+del screenshots.png
+del crudapp.py
+del _config.yml
+echo Files cleaned up successfully! 
